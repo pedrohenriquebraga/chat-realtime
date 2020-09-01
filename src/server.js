5abf7fd4c8bfac91fa9769c126c15d0bdc0e7eff
Pedro Henrique Santos Braga <gamesantos7@gmail.com>
2020-09-01 11:52:26 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useMongoClient: true
 });
 
 
