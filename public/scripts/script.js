8f34262267d14b659ab16a1e4d1841ec22504e4a
pedrohenriquebraga <gamesantos7@gmail.com>
2020-09-01 15:52:03 -0300

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,38 @@
-let messages = $("#messages")
-messages.prop('scrollTop', messages.prop('scrollHeight'))
+let messages = document.querySelector("#messages")
+messages.scrollBy(0, messages.scrollHeight)
 
 let socket = io("https://livechat-realtime.herokuapp.com/");
 
+if (Notification.permission !== 'granted') {
+    alert('Para receber notificações de novas mensagens permita que o site envie notificações!!')
+    Notification.requestPermission()
+}
 
-function sendNotification(options) {
+function newDate() {
+    const newDate = new Date();
+    let hours = {
+        hour: newDate.getHours().toString(),
+        minute: newDate.getMinutes().toString(),
+        second: newDate.getSeconds().toString(),
+    }
+
+    let dayAndMonth = {
+        day: newDate.getDate(),
+        month: newDate.getMonth() + 1
+    }
+
+    hours.hour = hours.hour <= 9 ? '0' + hours.hour : hours.hour
+    hours.minute = hours.minute <= 9 ? '0' + hours.minute : hours.minute
+    hours.second = hours.second <= 9 ? '0' + hours.second : hours.second
+
+    dayAndMonth.day = dayAndMonth.day <= 9 ? '0' + dayAndMonth.day : dayAndMonth.day
+    dayAndMonth.month = dayAndMonth.month <= 9 ? '0' + dayAndMonth.month : dayAndMonth.month
+
+    return `${hours.hour}:${hours.minute}:${hours.second} (${dayAndMonth.day}/${dayAndMonth.month})`
+
+}
+
+async function sendNotification(options) {
     let notify = new Notification(options.title, options.opt)
     if (Notification.permission == 'granted') {
         if (options.link !== '') {
@@ -17,7 +45,7 @@ function sendNotification(options) {
     }
 }
 
-function renderMessage(message) {
+async function renderMessage(message) {
 
     let converter = new showdown.Converter({
         noHeaderId: true,
@@ -33,21 +61,30 @@ function renderMessage(message) {
 
     message.message = await converter.makeHtml(message.message)
 
-    messages.append(`<div class="message"><strong class="name">${message.author}</strong>${message.message}<span id="date">${message.hour}</span></div>`);
+    messages.innerHTML += `<div class="message"><strong class="name">${message.author}</strong>${message.message}<span id="date">${message.hour}</span></div>`;
 
-    messages.prop('scrollTop', messages.prop('scrollHeight'))
+    messages.scrollBy(0, messages.scrollHeight)
 
 }
-function stripHTML(text) {
+
+async function stripHTML(text) {
     return text.replace(/<.*?>/gim, '').replace(/^#/gim, '\\#')
 }
 
-socket.on("receivedMessage", (message) => {
+socket.on("receivedMessage", async message => {
     renderMessage(message);
+    sendNotification({
+        opt: {
+            body: `Nova mensagem de ${message.author}`,
+            icon: '/assets/livechat-icon.png'
+        },
+        title: 'Live Chat',
+        link: 'https://livechat-realtime.herokuapp.com/'
+    })
 });
 
-socket.on("previousMessage", (messages) => {
-    $('#messages').text('')
+socket.on("previousMessage", async messages => {
+    document.querySelector('#messages').value = ''
     if (messages.length > 0) {
         for (message of messages) {
             renderMessage(message);
@@ -57,12 +94,12 @@ socket.on("previousMessage", (messages) => {
 
 });
 
-$("#chat").submit(function (event) {
+document.querySelector("#chat").addEventListener('submit', async event => {
     event.preventDefault();
 
-    let author = stripHTML($("#username").val());
-    let message = stripHTML($("#sendMessage").val());
-    $("#sendMessage").val("");
+    let author = await stripHTML(document.querySelector("#username").value);
+    let message = await stripHTML(document.querySelector("#sendMessage").value);
+    document.querySelector("#sendMessage").value = '';
 
     if (author.length && message.length) {
 
@@ -75,5 +112,5 @@ $("#chat").submit(function (event) {
         };
     }
     renderMessage(messageObj);
-    socket.emit("sendMessage", messageObj);
+    await socket.emit("sendMessage", messageObj);
 });
